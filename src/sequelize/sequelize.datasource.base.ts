3fcf68fbd0f70be809b9634232983e31b8c42705
Shubham Prajapat <shubham.prajapat@sourcefuse.com>
2022-10-18 12:35:42 +0530

    feat(datasource): add sequelize datasource base class
    exports `SequelizeDataSource` base class to be used as parent of datasources in target loopback 4
    project

---
+++ src/sequelize/sequelize.datasource.base.ts
@@ -0,0 +1,35 @@
+import {LifeCycleObserver} from '@loopback/core';
+import {juggler} from '@loopback/repository';
+import {Options as SequelizeOptions, Sequelize} from 'sequelize';
+
+export class SequelizeDataSource
+  extends juggler.DataSource
+  implements LifeCycleObserver
+{
+  constructor(public config: SequelizeOptions) {
+    super(config);
+  }
+  sequelize?: Sequelize;
+  async init(...injectedArgs: unknown[]): Promise<void> {
+    console.log('injected args', injectedArgs);
+    console.log('init called');
+    this.sequelize = new Sequelize({
+      host: this.config.host,
+      port: this.config.port,
+      database: this.config.database,
+      dialect: 'postgres',
+      username: this.config.username,
+      password: this.config.password,
+      logging: console.log,
+    });
+    try {
+      await this.sequelize.authenticate();
+      console.log('Connection has been established successfully.');
+    } catch (error) {
+      console.error('Unable to connect to the database:', error);
+    }
+  }
+  stop() {
+    this.sequelize?.close?.().catch(console.log);
+  }
+}
