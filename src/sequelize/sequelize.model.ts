f47382f04852b0d1c77bb440556cb2d22d2231aa
Shubham Prajapat <shubham.prajapat@sourcefuse.com>
2022-10-29 16:28:09 +0530

    refactor: add todo note in sequelize.model file

--- src/sequelize/sequelize.model.ts
+++ src/sequelize/sequelize.model.ts
@@ -3,12 +3,15 @@ import {Model} from 'sequelize';
 
 export class SequelizeModel extends Model implements Entity {
   getId() {
+    // TODO: Implement this Method
     return null;
   }
   getIdObject(): Object {
+    // TODO: Implement this Method
     return {};
   }
   toObject(options?: AnyObject | undefined): Object {
+    // TODO: Implement this Method
     return {};
   }
 }
