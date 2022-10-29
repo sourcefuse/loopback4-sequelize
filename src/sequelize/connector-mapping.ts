18200f8cdffa8aeb7f1564e757a26b902ec850ab
Shubham Prajapat <shubham.prajapat@sourcefuse.com>
2022-10-29 18:33:42 +0530

    refactor(datasource): add check for supported connectors

---
+++ src/sequelize/connector-mapping.ts
@@ -0,0 +1,16 @@
+import {Dialect as AllSequelizeDialects} from 'sequelize';
+
+export type SupportedLoopbackConnectors = "mysql" | "postgresql" | "oracle" | "sqlite3" | "db2";
+/**
+ * @key Loopback connectors name supported by this extension
+ * @value Equivalent Dialect in Sequelize
+ */
+export const SupportedConnectorMapping: {
+  [key in SupportedLoopbackConnectors]?: AllSequelizeDialects;
+} = {
+  "mysql": "mysql",
+  "postgresql": "postgres",
+  "oracle": "oracle",
+  "sqlite3" : "sqlite",
+  "db2": "db2",
+};
--- src/sequelize/sequelize.datasource.base.ts
+++ src/sequelize/sequelize.datasource.base.ts
@@ -1,23 +1,35 @@
 import {LifeCycleObserver} from '@loopback/core';
 import {juggler} from '@loopback/repository';
 import {Options as SequelizeOptions, Sequelize} from 'sequelize';
+import {
+  SupportedConnectorMapping as supportedConnectorMapping,
+  SupportedLoopbackConnectors,
+} from './connector-mapping';
 
 export class SequelizeDataSource
   extends juggler.DataSource
   implements LifeCycleObserver
 {
-  constructor(public config: SequelizeOptions & {user?: string}) {
+  constructor(public config: SequelizeDataSourceConfig) {
     super(config);
+
+    if (!(this.config.connector in supportedConnectorMapping)) {
+      throw new Error(
+        `Specified connector ${
+          this.config.connector ?? this.config.dialect
+        } is not supported.`,
+      );
+    }
   }
+
   sequelize?: Sequelize;
-  async init(...injectedArgs: unknown[]): Promise<void> {
-    console.log('injected args', injectedArgs);
-    console.log('init called');
+  async init(): Promise<void> {
     this.sequelize = new Sequelize({
       host: this.config.host,
       port: this.config.port,
       database: this.config.database,
-      dialect: 'postgres',
+      dialect:
+        supportedConnectorMapping[this.config.connector ?? this.config.dialect],
       username: this.config.user ?? this.config.username,
       password: this.config.password,
       logging: console.log,
@@ -33,3 +45,8 @@ export class SequelizeDataSource
     this.sequelize?.close?.().catch(console.log);
   }
 }
+
+export type SequelizeDataSourceConfig = SequelizeOptions & {
+  user?: string;
+  connector: SupportedLoopbackConnectors;
+};
