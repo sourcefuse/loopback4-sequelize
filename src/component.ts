926dae44befc77a1eacb191a7a8ceb3a04cc50bd
Shubham Prajapat <shubham.prajapat@sourcefuse.com>
2023-01-03 12:51:50 +0530

    refactor(ci-cd): remove prettier check
    GH-0

--- src/component.ts
+++ src/component.ts
@@ -1,22 +1,10 @@
-import {
-  Application,
-  injectable,
-  Component,
-  config,
-  ContextTags,
-  CoreBindings,
-  inject,
-} from '@loopback/core';
-import {LB4SequelizeComponentBindings} from './keys'
-import {DEFAULT_LOOPBACK4_SEQUELIZE_OPTIONS, LB4SequelizeComponentOptions} from './types';
+import {Component, ContextTags, injectable} from '@loopback/core';
+import {LB4SequelizeComponentBindings} from './keys';
 
 // Configure the binding for LB4SequelizeComponent
-@injectable({tags: {[ContextTags.KEY]: LB4SequelizeComponentBindings.COMPONENT}})
+@injectable({
+  tags: {[ContextTags.KEY]: LB4SequelizeComponentBindings.COMPONENT},
+})
 export class LB4SequelizeComponent implements Component {
-  constructor(
-    @inject(CoreBindings.APPLICATION_INSTANCE)
-    private application: Application,
-    @config()
-    private options: LB4SequelizeComponentOptions = DEFAULT_LOOPBACK4_SEQUELIZE_OPTIONS,
-  ) {}
+  constructor() {}
 }
